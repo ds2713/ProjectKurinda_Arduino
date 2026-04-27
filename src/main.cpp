c90c4bbb3bb66c341fb92e520145d69a2f64774c
David Salmon <dsalmon@magdrivespace.com>
2026-04-28 00:10:50 +0100

    Added low power sleep library. Changed the main code architecture so that the Arduino wakes, collects data every second, then returns to low power sleep mode. After 6 collections, the WiFi module turns on, transmits data, then turns off again to save power.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <WiFiNINA.h>
 #include <PubSubClient.h>
+#include <ArduinoLowPower.h>
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
@@ -17,6 +18,8 @@ const int mqtt_port = 1883;
 // ==== SAMPLE TIMING CONGIGURATION ====
 
 const int SAMPLE_COUNT = 6;
+const int TX_COUNT_MAX = 6; // Transmit every 6 samples (1 min)
+const int SLEEP_INTERVAL_MS = 1000; // 10s
 
 float acc_buffer[SAMPLE_COUNT] = {0};
 float temp_buffer[SAMPLE_COUNT] = {0};
@@ -28,12 +31,10 @@ float soil_sum = 0;
 
 int buffer_index = 0;
 int samples_collected = 0;
+int tx_counter = 0;
 
 unsigned long last_sample_time = 0;
 unsigned long last_publish_time = 0;
-
-const unsigned long SAMPLE_INTERVAL = 1000;  // 10s
-const unsigned long PUBLISH_INTERVAL = 6000; // 60s
 
 // ==== SENSOR CONFIGURATION ====
 
@@ -62,7 +63,7 @@ float get_soil_moisture() {
 
 // ==== CONNECT TO WIFI ====
 void connectWiFi() {
-  Serial.print("Connecting to WiFi...");
+  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -89,107 +90,135 @@ void connectMQTT() {
   }
 }
 
-void setup() {
-  Serial.begin(9600);
-  delay(2000);
+void transmit_data(){
+  
+  Serial.println("Publishing data...");
 
   connectWiFi();
 
   client.setServer(mqtt_server, mqtt_port);
-}
 
-void loop() {
   if (!client.connected()) {
     connectMQTT();
   }
 
   client.loop();
-
+    
   unsigned long now = millis();
 
-  // ==== SAMPLE EVERY 10s ====
-  if (now - last_sample_time >= SAMPLE_INTERVAL) {
-    last_sample_time = now;
+  float acc_avg = acc_sum / samples_collected;
+  float temp_avg = temp_sum / samples_collected;
+  float soil_avg = soil_sum / samples_collected;
 
-    float new_acc = get_acceleration();
-    float new_temp = get_temperature();
-    float new_soil = get_soil_moisture();
+  Serial.print("Avg acceleration: ");
+  Serial.println(acc_avg);
 
-    // subtract old values (only once buffer is full)
-    if (samples_collected == SAMPLE_COUNT) {
-      acc_sum -= acc_buffer[buffer_index];
-      temp_sum -= temp_buffer[buffer_index];
-      soil_sum -= soil_buffer[buffer_index];
-    }
+  Serial.print("Avg temp: ");
+  Serial.println(temp_avg);
 
-    // store new values
-    acc_buffer[buffer_index] = new_acc;
-    temp_buffer[buffer_index] = new_temp;
-    soil_buffer[buffer_index] = new_soil;
+  Serial.print("Avg SM: ");
+  Serial.println(soil_avg);
 
-    // add to sums
-    acc_sum += new_acc;
-    temp_sum += new_temp;
-    soil_sum += new_soil;
+  String payload = "{";
+  payload += "\"sid\":" + String(site_id) + ",";
+  payload += "\"ts\":" + String(now) + ",";
+  payload += "\"ac_avg\":" + String(acc_avg) + ",";
+  payload += "\"tmp_avg\":" + String(temp_avg) + ",";
+  payload += "\"sm_avg\":" + String(soil_avg);
+  payload += "}";
 
-    Serial.print("New acceleration: ");
-    Serial.println(new_acc);
+  client.publish(data_topic, payload.c_str());
 
-    Serial.print("New temp: ");
-    Serial.println(new_temp);
+  Serial.print("Published avg: ");
+  Serial.println(payload);
 
-    Serial.print("New SM: ");
-    Serial.println(new_soil);
+  Serial.println("Turning off WiFi to save power...");
 
-    Serial.print("Sum acceleration: ");
-    Serial.println(acc_sum);
+  WiFi.end(); // disconnect WiFi to save power
 
-    Serial.print("Sum temp: ");
-    Serial.println(temp_sum);
-
-    Serial.print("Sum SM: ");
-    Serial.println(soil_sum);
-
-    // move buffer index
-    buffer_index = (buffer_index + 1) % SAMPLE_COUNT;
-
-    if (samples_collected < SAMPLE_COUNT) {
-      samples_collected++;
-    }
-
-    Serial.println("Sample collected");
-  }
-
-  // ==== PUBLISH EVERY 60s ====
-  if (now - last_publish_time >= PUBLISH_INTERVAL && samples_collected > 0) {
-    last_publish_time = now;
-
-    float acc_avg = acc_sum / samples_collected;
-    float temp_avg = temp_sum / samples_collected;
-    float soil_avg = soil_sum / samples_collected;
-
-    Serial.print("Avg acceleration: ");
-    Serial.println(acc_avg);
-
-    Serial.print("Avg temp: ");
-    Serial.println(temp_avg);
-
-    Serial.print("Avg SM: ");
-    Serial.println(soil_avg);
-
-    String payload = "{";
-    payload += "\"sid\":" + String(site_id) + ",";
-    payload += "\"ts\":" + String(now) + ",";
-    payload += "\"ac_avg\":" + String(acc_avg) + ",";
-    payload += "\"tmp_avg\":" + String(temp_avg) + ",";
-    payload += "\"sm_avg\":" + String(soil_avg);
-    payload += "}";
-
-    client.publish(data_topic, payload.c_str());
-
-    Serial.print("Published avg: ");
-    Serial.println(payload);
-  }
 }
 
+void data_processing_loop(){
 
+  Serial.println("Starting data processing loop...");
+
+  // ==== SAMPLE EVERY 10s ====
+
+  float new_acc = get_acceleration();
+  float new_temp = get_temperature();
+  float new_soil = get_soil_moisture();
+
+  // subtract old values (only once buffer is full)
+  if (samples_collected == SAMPLE_COUNT) {
+    acc_sum -= acc_buffer[buffer_index];
+    temp_sum -= temp_buffer[buffer_index];
+    soil_sum -= soil_buffer[buffer_index];
+  }
+
+  // store new values
+  acc_buffer[buffer_index] = new_acc;
+  temp_buffer[buffer_index] = new_temp;
+  soil_buffer[buffer_index] = new_soil;
+
+  // add to sums
+  acc_sum += new_acc;
+  temp_sum += new_temp;
+  soil_sum += new_soil;
+
+  Serial.print("New acceleration: ");
+  Serial.println(new_acc);
+
+  Serial.print("New temp: ");
+  Serial.println(new_temp);
+
+  Serial.print("New SM: ");
+  Serial.println(new_soil);
+
+  Serial.print("Sum acceleration: ");
+  Serial.println(acc_sum);
+
+  Serial.print("Sum temp: ");
+  Serial.println(temp_sum);
+
+  Serial.print("Sum SM: ");
+  Serial.println(soil_sum);
+
+  // move buffer index
+  buffer_index = (buffer_index + 1) % SAMPLE_COUNT;
+
+  if (samples_collected < SAMPLE_COUNT) {
+    samples_collected++;
+  }
+
+  Serial.println("Sample collected");
+
+  // ==== PUBLISH EVERY 60s ====
+  if (tx_counter >= TX_COUNT_MAX) {
+    tx_counter = 0;
+    transmit_data();
+  } else {
+    tx_counter++;
+    Serial.print("TX counter: ");
+    Serial.println(tx_counter);
+  }
+
+  return;
+
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+}
+
+void loop() {
+
+
+  while(1) {
+    data_processing_loop();
+
+    // delay(SLEEP_INTERVAL_MS);
+     LowPower.sleep(SLEEP_INTERVAL_MS);
+  }
+
+}
